801d67087bb1fe8bfdd76c877e2b42eaa34ab1f4
Dhanush123 <dhanush.patel@ymail.com>
2018-01-20 22:25:08 -0800

    trying avg

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ const request = require("request");
 const server = express();
 server.use(bodyParser.json());
 
+var config = {
+  apiKey: "AIzaSyC9SEhQV-_TRFoSDomQPQga0ms2l607h6c",
+  authDomain: "env-ai.firebaseapp.com",
+  databaseURL: "https://env-ai.firebaseio.com",
+  projectId: "env-ai",
+  storageBucket: "env-ai.appspot.com",
+  messagingSenderId: "49236414526"
+};
+
+// Get a reference to the database service
+var database;
+
 server.post("/", function (req, res) {
   console.log("webhook request");
   try {
@@ -35,8 +47,19 @@ server.post("/", function (req, res) {
 var functions = {sensorAverage,sensorCurrent,sensorTrend};
 
 function sensorAverage(rez, paramz) {
-  return rez.json({
-    "speech": JSON.stringify(paramz)
+  firebase.database().ref('/sensor').once('value').then(function(snapshot) {
+    var average = 0;
+    var numEntries = 0;
+    var sensorData = snapshot.val();
+    console.log("sensorData",sensorData);
+    for (var entry in sensorData) {
+      average += entry[paramz["sensor"]];
+      numEntries += 1;
+    }
+    average = average/numEntries;
+    return rez.json({
+      "speech": "The average "+paramz["sensor"]+" is "+average
+    }); 
   });
 }
 
@@ -54,4 +77,6 @@ function sensorTrend(rez, paramz) {
 
 server.listen((process.env.PORT || 8000), function () {
   console.log("chatbot server is up!!!!!!!!!!!!!!!!!!!!");
+  firebase.initializeApp(config);
+  database = firebase.database();
 });
